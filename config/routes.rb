8af71302297c3f7856d557ec0bb021012ecca7b1
PRATIK DHARAMDASANI <pratik@commutatus.com>
2020-02-20 09:35:32 +0530

    rootpage set

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   end
   resources :accounts
   resources :employees
+  root :to => 'employees#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
