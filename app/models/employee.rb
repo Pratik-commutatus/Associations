2b1f1518c6fbf2e249930d19518aa3b649d3c2ab
PRATIK DHARAMDASANI <pratik@commutatus.com>
2020-02-24 18:21:32 +0530

    has one fix

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,4 @@
 class Employee < ApplicationRecord
     has_one :account
-    accepts_nested_attributes_for :account, reject_if: lambda {|attributes| attributes['bookname'].blank? }
+    accepts_nested_attributes_for :account, reject_if: lambda {|attributes| attributes[:number].blank? }
 end
